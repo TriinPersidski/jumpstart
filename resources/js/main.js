8b97ecc799032f75956feccf4c7b244bbbedffbb
Triin Persidski <triin@positive.com>
2018-01-09 16:13:50 +1100

    Adding carousel

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   $('.carousel').slick({
-    autoplay: true;
-    autoplaySpeed: 4000;
     arrows: false;
     draggable: false;
     pauseOnFocus: false;
     pauseOnHover: false;
-  });
+    slidesToShow: 1,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
 });
